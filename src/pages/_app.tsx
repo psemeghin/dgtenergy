0563eac439e8ec9f2f3817c3ada0572578948f00
psemeghin <pedrohmsemeghini@gmail.com>
2025-08-26 05:28:01 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+// src/pages/_app.tsx
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-white text-gray-900">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
