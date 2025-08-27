30b3d64198125496975bd45c54c3bec734a0fb96
psemeghin <pedrohmsemeghini@gmail.com>
2025-08-27 22:03:00 +0200

    Update connect.tsx

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,14 +1,17 @@
-import Head from 'next/head';
+// src/pages/connect.tsx
+import MetaHead from "../components/MetaHead";
+import ConnectIntro from "../components/ConnectIntro";
+import WalletOptions from "../components/WalletOptions";
+import SecurityNotes from "../components/SecurityNotes";
 
 export default function ConnectPage() {
   return (
     <>
-      <Head>
-        <title>Connect | DGTEnergy</title>
-      </Head>
-      <main className="min-h-screen p-8">
-        <h1 className="text-4xl font-bold text-center">Connect Page</h1>
-        <p className="mt-4 text-center text-gray-500">This is the connect page of the DGTEnergy DApp.</p>
+      <MetaHead />
+      <main className="bg-[#FAFAFA] text-gray-900">
+        <ConnectIntro />
+        <WalletOptions />
+        <SecurityNotes />
       </main>
     </>
   );
