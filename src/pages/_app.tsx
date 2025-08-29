3e9666b2a230615f8020399de22a02c48eee8b2e
psemeghin <pedrohmsemeghini@gmail.com>
2025-08-29 02:41:41 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,13 @@ import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { wagmiConfig, chains } from '../lib/wallet';
 import '@rainbow-me/rainbowkit/styles.css';
 
+import Header from '../src/components/Header';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
+        <Header />
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
