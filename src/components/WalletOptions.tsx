8c81c5a3c9221417435712d15a9929d6c833f053
psemeghin <pedrohmsemeghini@gmail.com>
2025-09-06 04:03:00 +0200

    Update WalletOptions.tsx

--- src/components/WalletOptions.tsx
+++ src/components/WalletOptions.tsx
@@ -1,7 +1,7 @@
 // src/components/WalletOptions.tsx
 'use client';
 
-import { useAccount } from 'wagmi/hooks';
+import { useAccount } from 'wagmi';
 
 export default function WalletOptions() {
   const { address, isConnected } = useAccount();
