09fabbc083487fa72ff01322cd941c7e7ff38ffc
psemeghin <pedrohmsemeghini@gmail.com>
2025-09-06 03:02:13 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 // src/pages/_app.tsx
+
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
 import { WagmiConfig } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, darkTheme, lightTheme } from '@rainbow-me/rainbowkit';
+
 import { wagmiConfig, chains } from '../lib/wallet';
 import '@rainbow-me/rainbowkit/styles.css';
 
@@ -10,11 +14,27 @@ import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Header />
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>DGT-Energy Token Sale</title>
+      </Head>
+
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={lightTheme({
+            accentColor: '#facc15', // amarelo padrão DGT (tailwind yellow-400)
+            accentColorForeground: 'black',
+            borderRadius: 'medium',
+          })}
+          modalSize="compact"
+          showRecentTransactions={true}
+        >
+          <Header />
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
   );
 }
