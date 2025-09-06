4952fbe8204abf7dff1ecb473f04d060ebd02b89
Pedro Semeghini <pedrosemeghini@MacBook-Air.local>
2025-09-06 07:26:06 +0200

    fix: ajuste em wagmi.config.ts e dependências atualizadas

--- src/wagmi.config.ts
+++ src/wagmi.config.ts
@@ -1,17 +1,11 @@
-// src/wagmi.config.ts
-
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { bsc } from 'wagmi/chains';
-import { createConfig } from 'wagmi';
-import { publicClient, webSocketPublicClient } from 'wagmi/clients';
+import { configureChains, createConfig } from 'wagmi';
+import { mainnet, bsc } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
 
-export const chains = [bsc];
-
-export const config = createConfig(
-  getDefaultConfig({
-    appName: 'DGT-Energy Token Sale',
-    projectId: 'a6bc7e0b017a6a0765d022bf838de533', // ← Troque pelo seu ID da WalletConnect
-    chains,
-    ssr: false,
-  })
-);
+export const config = getDefaultConfig({
+  appName: 'DGT–Energy Token Sale',
+  projectId: 'a6bc7e0b017a6a0765d022bf838de533',
+  chains: [bsc],
+  ssr: false,
+});
