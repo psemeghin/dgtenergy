2c73a800c50d44d87a05f26850d435e31469d20d
psemeghin <pedrohmsemeghini@gmail.com>
2025-09-06 04:35:17 +0200

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,15 +1,19 @@
-// src/wagmi.ts
-import { configureChains, createConfig } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import { bsc } from 'wagmi/chains';
+// wagmi.config.ts
+import { http, createConfig } from 'wagmi'
+import { bsc } from 'wagmi/chains'
+import { injected, walletConnect, coinbaseWallet } from '@wagmi/connectors'
 
-const { chains, publicClient } = configureChains(
-  [bsc],
-  [publicProvider()],
-);
-
-export const wagmiConfig = createConfig({
-  autoConnect: true,
-  publicClient,
-  chains,
-});
+export const config = createConfig({
+  chains: [bsc],
+  connectors: [
+    injected(),
+    walletConnect({
+      projectId: 'SEU_PROJECT_ID_DA_WALLETCONNECT',
+    }),
+    coinbaseWallet({ appName: 'DGTEnergy' }),
+  ],
+  transports: {
+    [bsc.id]: http(),
+  },
+  ssr: true,
+})
